946247fe918bf38191c9dea7870c9d6436844a7f
boris.perkovic@p3-group.com <boris.perkovic@p3-group.com>
2022-08-05 13:56:29 +0200

    Update hero banner image

--- src/hero-banner/HerroBanner.tsx
+++ src/hero-banner/HerroBanner.tsx
@@ -1,18 +1,17 @@
 import { Box, Container, Grid } from "@mui/material/";
 import { useTranslation } from "react-i18next";
-import BannerImage from "../assets/images/FE.png";
+import BannerImage from "../assets/images/FE_dark.png";
 import { ECText } from "../components/ECText";
 import { useAppTheme } from "../theme/useAppTheme";
 
 const styles = {
   bgImage: {
+    paddingTop: "20%",
+    paddingBottom: "20%",
     backgroundImage: `url(${BannerImage})`,
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-  },
-  text: {
-    height: "80vh",
   },
 };
 
@@ -23,15 +22,14 @@ export const HerroBanner = () => {
   return (
     <Box component={"header"} bgcolor={palette.grey[200]}>
       <Container maxWidth="xl">
-        <Grid container paddingY={2}>
-          <Grid item xs={7} style={styles.bgImage} />
+        <Grid container paddingY={3}>
+          <Grid item xs={6} style={styles.bgImage}></Grid>
           <Grid
             display={"flex"}
             flexDirection="column"
             justifyContent={"center"}
             item
-            xs={5}
-            style={styles.text}
+            xs={6}
           >
             <ECText component="p" fontSize={16} textAlign="center">
               {t("shopAndService")}
